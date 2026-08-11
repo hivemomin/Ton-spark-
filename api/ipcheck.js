53965ca9813a5a8e09bcf786577b7d06edc486e8
hivemomin <hiviof@gmail.com>
2026-08-11 08:01:44 +0600

    Update ipcheck.js

--- api/ipcheck.js
+++ api/ipcheck.js
@@ -18,7 +18,7 @@ function getClientIp(req) {
 }
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ton-spark-beta.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ton-spark-qu47.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
