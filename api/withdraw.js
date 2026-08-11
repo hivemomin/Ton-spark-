9c0228f565247efee968decc2e4278049fed72b3
hivemomin <hiviof@gmail.com>
2026-08-11 13:50:52 +0600

    Update withdraw.js

--- api/withdraw.js
+++ api/withdraw.js
@@ -12,7 +12,7 @@ const MIN_TASKS_COMPLETED = 5;
 const MIN_LIGHTNING_CLAIMS = 1;
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://ton-spark-beta.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://ton-spark-qu47.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
